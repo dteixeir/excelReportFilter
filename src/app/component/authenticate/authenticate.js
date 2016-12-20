e57f89aec657e093500b9093dfebba543dd711cc
danny teixeira <dteixeira@ip-192-168-1-109.ec2.internal>
2016-12-19 22:45:29 -0500

    table populating - flexbox still wrong

--- src/app/component/authenticate/authenticate.js
+++ src/app/component/authenticate/authenticate.js
@@ -12,32 +12,20 @@ angular.module('clientApp.component.authenticate')
     var vm = this;
 
     // functions
-    vm.submit = submit;
-    vm.print = print;
     vm.getData = getData;
-    vm.stuff;
-
+    vm.tableStart = 4;
+    vm.data = getData();    
 
     $window.addEventListener('dragend', function (event) {
       event.preventDefault();
       return false;
     }, false);
 
-
-    function submit() {
-      vm.auth = {
-        username: vm.username,
-        password: vm.password
-      };
-    }
-
-    function print() {
-      getData();
-    }
-
     function getData() {
-      $http.get(__dirname + '/write.json').then(function(response) {
-        vm.stuff = response.data;
+      return $http.get(__dirname + '/write.json').then(function(response) {
+        vm.data = response.data;
+        vm.headers = vm.data[0];
+        vm.data.shift();
       }, function() {
           throw 'There was an error getting data';
       });
