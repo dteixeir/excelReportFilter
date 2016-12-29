d015c2eda608c141404deffa361fbc17050b65e5
danny teixeira <dteixeira@dannys-MBP.attlocal.net>
2016-12-28 23:34:12 -0500

    Checklist affects table

--- src/app/component/authenticate/authenticate.js
+++ src/app/component/authenticate/authenticate.js
@@ -13,8 +13,16 @@ angular.module('clientApp.component.authenticate')
 
     // functions
     vm.getData = getData;
+    vm.print = print;
+    vm.contains = contains;
+    vm.table = false;
+
     vm.tableStart = 4;
-    vm.data = getData();    
+    vm.headers2 = [];
+    vm.indexs = [];
+
+    vm.data = getData();
+    
 
     $window.addEventListener('dragend', function (event) {
       event.preventDefault();
@@ -25,9 +33,36 @@ angular.module('clientApp.component.authenticate')
       return $http.get(__dirname + '/write.json').then(function(response) {
         vm.data = response.data;
         vm.headers = vm.data[0];
+        upgradeHeaders();
         vm.data.shift();
       }, function() {
           throw 'There was an error getting data';
       });
+    }
+
+    function upgradeHeaders() {
+      vm.headers.forEach((element) => {
+        var item = { text: element, value: false, index: null };
+        vm.headers2.push(item);
+      });
+    }
+
+    function print() {
+      vm.headers2.forEach((element) => {
+        if (element.index) {
+          vm.indexs.push(element.index);
+        }
+      });
+      vm.table = true;
+    }
+
+    function contains(index) {
+      for (var i = 0; i < vm.indexs.length; i++) {
+        if (vm.indexs[i] === index) {
+          return true;
+        }
+      }
+
+      return false;
     }
   });
